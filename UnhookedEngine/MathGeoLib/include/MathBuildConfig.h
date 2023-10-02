c2d8f711ffcc148b67211a708703f3a4f6202d68
David Ruiz Luengo <davidruizluengo@gmail.com>
2023-10-02 08:44:11 +0200

    Ypdate MathGeoLibB

--- UnhookedEngine/MathGeoLib/include/MathBuildConfig.h
+++ UnhookedEngine/MathGeoLib/include/MathBuildConfig.h
@@ -1,4 +1,4 @@
-/* Copyright 2011 Jukka Jylï¿½nki
+/* Copyright 2011 Jukka Jylänki
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
    limitations under the License. */
 
 /** @file MathBuildConfig.h
-	@author Jukka Jylï¿½nki
+	@author Jukka Jylänki
 	@brief Specifies all build flags for the library. */
 #pragma once
 
@@ -26,7 +26,7 @@
 #pragma warning( disable : 4577 ) // Warning that exceptions are disabled
 #pragma warning( disable : 4530 ) // Warning that exceptions are disabled
 #define MATH_SILENT_ASSUME
-//#define MATH_BULLET_INTEROP
+#define MATH_BULLET_INTEROP
 // ---
 
 // Disable STL exceptions
@@ -123,7 +123,7 @@
 #endif
 
 // Uncomment to specify the SIMD instruction set level in use.
-//#define MATH_AVX
+#define MATH_AVX
 //#define MATH_SSE41
 //#define MATH_SSE3
 //#define MATH_SSE2
